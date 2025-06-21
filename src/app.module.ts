98729c4b1184ca90bad4a09ac7046ff7d072727b
DelioCoder <davideveriwhere@gmail.com>
2025-06-20 19:39:36 -0500

    FilesModule added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+// import { join } from 'path';
+// import { ServeStaticModule } from '@nestjs/serve-static';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
 
 
 @Module({
@@ -19,9 +22,13 @@ import { SeedModule } from './seed/seed.module';
       autoLoadEntities: true,
       synchronize: true,
     }),
+    // ServeStaticModule.forRoot({
+    //   rootPath: join(__dirname,'..','public'),
+    // }),
     ProductsModule,
     CommonModule,
-    SeedModule
+    SeedModule,
+    FilesModule,
   ],
   controllers: [],
   providers: [],
